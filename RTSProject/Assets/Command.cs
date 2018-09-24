c3d4b03e11ceda808cfd740a5bfc44950a90236d
nikitabm <1nikita.bal.2200@gmail.com>
2018-09-24 14:21:52 +0200

    command class

---
+++ RTSProject/Assets/Command.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class Command : MonoBehaviour
+{
+
+    public enum ECommandType
+    {
+        CM_INVALID,
+        CM_ATTACK,
+        CM_MOVE
+    };
+    public virtual void ProcessCommand()
+	{
+
+	}
+
+}
+public class SelectCommand : Command
+{
+	public override void ProcessCommand()
+	{
+
+	}
+
+}
