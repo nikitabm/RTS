b91ba8649881acc92df9c12fb73c5fbbcc358240
Nikita <nikitabalabuiev@gmail.com>
2018-12-18 00:25:23 +0100

    stuff

--- RTSProject/Assets/Scripts/UnitRelatedScripts/Unit.cs
+++ RTSProject/Assets/Scripts/UnitRelatedScripts/Unit.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Unit : MonoBehaviour, ISelectable
 {
     [SerializeField]
-    private Command _currentCommand;
+    private Command _currentCommand = new MoveCommand(new List<int> { -1 }, new Vector3(1, 1, 1));
     private Queue<Command> _commandQueue = new Queue<Command>();
+    private IEnumerator coroutine;
     public Command CurrentCommand
     {
         get
@@ -32,9 +33,51 @@ public class Unit : MonoBehaviour, ISelectable
            gameObject.transform.position.y,
           Mathf.Round(gameObject.transform.position.z));
     }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            ExecuteCurrentCommand();
+        }
+    }
     public void ExecuteCurrentCommand()
     {
-        if (CurrentCommand != null)
-            this.gameObject.transform.position = _currentCommand.position;
+        if (_currentCommand != null)
+        {
+            var movePoint = new Vector3(
+                Mathf.Round(_currentCommand.position.x),
+                gameObject.transform.position.y,
+                Mathf.Round(_currentCommand.position.z));
+            StartCoroutine(AllignOnX(movePoint));
+        }
+    }
+    private IEnumerator AllignOnX(Vector3 movePoint)
+    {
+        int step;
+        int pX = (int)movePoint.x;
+        int numberOfSteps = (int)transform.position.x - pX;
+        if (numberOfSteps > 0) step = -1;
+        else step = 1;
+
+        for (int i = 0; i <= Mathf.Abs(numberOfSteps); i++)
+        {
+            yield return new WaitForSeconds(1.0f);
+            gameObject.transform.position += new Vector3(step, 0, 0);
+        }
+        yield return StartCoroutine(AllignOnZ((int)movePoint.z));
+    }
+    private IEnumerator AllignOnZ(int pZ)
+    {
+
+        int step;
+        int numberOfSteps = (int)transform.position.z - pZ;
+        if (numberOfSteps > 0) step = -1;
+        else step = 1;
+
+        for (int i = 0; i <= Mathf.Abs(numberOfSteps); i++)
+        {
+            yield return new WaitForSeconds(1.0f);
+            gameObject.transform.position += new Vector3(0, 0, step);
+        }
     }
 }
