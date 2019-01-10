7f83e4ef2716bfd93b5bf53d191515a6e8929cf2
Nikita <nikitabalabuiev@gmail.com>
2019-01-10 16:56:23 +0100

    changed selection buttons

--- RTSProject/Assets/Scripts/SelectObject.cs
+++ RTSProject/Assets/Scripts/SelectObject.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class SelectObject : MonoBehaviour
 {
-
     private Vector3 _clickPoint = Vector3.zero;
     private List<Unit> _units = new List<Unit>();
-
+    private Vector3 _mousePosition;
+    private bool _dragging;
     [SerializeField]
     private bool _enabled;
     private int _playerID;
@@ -32,20 +33,28 @@ public class SelectObject : MonoBehaviour
     {
         Idle,
         SelectBuilding,
-        SelectUnit,
+        UnitSelected,
         SelectedLocation,
         MoveCommandComplete
     }
 
     public StateOfPlayer playerState;
-    void Start()
+    private void Start()
     {
         playerState = StateOfPlayer.Idle;
         ServiceLocator.GetService<CommandManager>().SubsribeToEvent();
     }
-    void Update()
+    private void Update()
     {
         ClickOnObjects();
+    }
+    private void OnGUI()
+    {
+        if (_dragging)
+        {
+            var rect = ScreenHelper.GetScreenRect(_mousePosition, Input.mousePosition);
+            ScreenHelper.DrawScreenRect(rect, Color.green);
+        }
     }
     public void SetEnabled(bool value)
     {
@@ -60,6 +69,7 @@ public class SelectObject : MonoBehaviour
         if (!_enabled) return;
         if (Input.GetMouseButtonDown(0))
         {
+            _mousePosition = Input.mousePosition;
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 1000f))
@@ -72,25 +82,41 @@ public class SelectObject : MonoBehaviour
                         _units.Clear();
                         var unit = obj.transform.gameObject.GetComponent<Unit>();
                         _units.Add(unit);
-                        playerState = StateOfPlayer.SelectUnit;
+                        playerState = StateOfPlayer.UnitSelected;
                         onSelected();
 
                     }
+                    else
+                    {
+                        _dragging = true;
+                    }
                 }
-                else if (playerState == StateOfPlayer.SelectUnit)
+                else if (playerState == StateOfPlayer.UnitSelected)
                 {
-                    _clickPoint = hit.point;
-                    playerState = StateOfPlayer.SelectedLocation;
-                    CreateAndPassCommand(_units[0].ID, _clickPoint);
+                    _units.Clear();
                     playerState = StateOfPlayer.Idle;
+                    //_clickPoint = hit.point;
+                    //playerState = StateOfPlayer.SelectedLocation;
+                    //CreateAndPassCommand(_units[0].ID, _clickPoint);
+                    //playerState = StateOfPlayer.Idle;
 
                 }
             }
         }
         if (Input.GetMouseButtonUp(1))
         {
-            _units.Clear();
-            playerState = StateOfPlayer.Idle;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                if (playerState == StateOfPlayer.UnitSelected)
+                {
+                    _clickPoint = hit.point;
+                    playerState = StateOfPlayer.SelectedLocation;
+                    CreateAndPassCommand(_units[0].ID, _clickPoint);
+                    playerState = StateOfPlayer.Idle;
+                }
+            }
         }
     }
     public void CreateAndPassCommand(int i, Vector3 pos)
@@ -104,7 +130,6 @@ public class SelectObject : MonoBehaviour
         if (ServiceLocator.GetService<GameManager>().movementWithoutNetwork)
         {
             ServiceLocator.GetService<CommandManager>().PassCommandsToUnits();
-            // ServiceLocator.GetService<CommandManager>()._allCommands.Clear();
         }
     }
 }
