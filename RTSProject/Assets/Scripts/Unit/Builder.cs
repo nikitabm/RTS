eb136379e646b006269704e47899c5dcaeb4caac
Nikita <nikitabalabuiev@gmail.com>
2019-01-25 15:31:55 +0100

    ui changes

--- RTSProject/Assets/Scripts/Unit/Builder.cs
+++ RTSProject/Assets/Scripts/Unit/Builder.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Builder : MonoBehaviour {
+public class Builder : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool _selected;
+    public bool Selected
+    {
+        get { return _selected; }
+        set { _selected = value; }
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
 }
